0c6ce0cdec39380fb2b913efbd8e92d6bee41977
ayushbachasa <ayushanoopbachasa@gmail.com>
2025-10-04 15:29:53 -0400

    Changed recorder to tsx and added agent functionality to recorder

--- src/app/interview/page.tsx
+++ src/app/interview/page.tsx
@@ -1,11 +1,9 @@
 import Recorder from "@/components/recorder";
-import { Conversation } from "@/components/conversation";
 
 export default function Interview() {
     return (
       <div className="font-sans">
         <Recorder />
-        <Conversation />
       </div>
   
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,6 @@ export default function Home() {
     <div className="font-sans">
       <HomePage />
       <Recorder />
-      <Conversation />
     </div>
 
   );
