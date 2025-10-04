649b605b565ab58ee30c762efd5dc9589edd8c68
Ryan Zhou <ryanzhou07@gmail.com>
2025-10-04 14:28:14 -0400

    added the conversation agent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Image from "next/image";
 import HomePage from "@/components/homepage";
 import Recorder from "@/components/recorder";
+import { Conversation } from "@/components/conversation";
 
 export default function Home() {
   return (
     <div className="font-sans">
       <HomePage />
       <Recorder />
+      <Conversation />
     </div>
 
   );
