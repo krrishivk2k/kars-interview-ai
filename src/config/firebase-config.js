7634a8da9a51294b0959c32c2f48e4bd9e077bee
ayushbachasa <ayushanoopbachasa@gmail.com>
2025-10-06 18:25:31 -0400

    Security Hotfix

--- src/config/firebase-config.js
+++ src/config/firebase-config.js
@@ -10,7 +10,7 @@ import { getFirestore } from "firebase/firestore";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY /* || "AIzaSyB8_dvrj05uAq2r66u9WDTABYadPFpE0l4" */,
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
     projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
